04c887bcba359fe2dcfbef93d7851ae3f11fceeb
Roxyhuang <sixin_huang@outlook.com>
2017-11-08 09:43:38 +0800

    update async load

--- src/components/include/cell/Cell.jsx
+++ src/components/include/cell/Cell.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import cny from 'assets/img/flags/cny.png';
 import s from './cell.less';
+
 
 const Cell = () => {
   return (
     <div className={s.container}>
-      <img src="" alt="" />
+      <img src={cny} alt="flags" className={s.flags} />
       <div>CNY</div>
       <div>
         <div>7.03</div>
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,10 @@
-import Index from '../components/containers/index/Index';
-import List from '../components/containers/list/List';
+import { asyncComponent } from '../utils/asyncComponent';
+
+// import Index from '../components/containers/index/Index';
+// import List from '../components/containers/list/List';
+
+const Index = asyncComponent(() => import('../components/containers/index/Index'));
+const List = asyncComponent(() => import('../components/containers/list/List'));
 
 const router = {
   'p/index.html': {
